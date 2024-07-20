b315795fd914dee06a72b58d6fad0a2117c6f947
Matt Rybin <contact@mattrybin.com>
2024-07-20 17:16:14 +0200

    chore: update release to only include mac

--- language/scripts/package.ts
+++ language/scripts/package.ts
@@ -1,8 +1,8 @@
 import { compress } from "https://deno.land/x/zip@v1.2.5/mod.ts"
 import { join } from "https://deno.land/std@0.188.0/path/mod.ts"
 
-const target = Deno.args[0]
-const filename = target === "x86_64-pc-windows-msvc" ? `epic-${target}.exe` : `epic-${target}`
+const target = "x86_64-apple-darwin"
+const filename = `epic-${target}`
 
 const distDir = "dist"
 const inputFile = join(distDir, filename)
